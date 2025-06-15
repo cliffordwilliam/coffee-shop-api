3bee9a3d37aa0c10e5caa92d7ce3435663398d5f
Clifford William <ccliffordwilliam@gmail.com>
2025-06-16 00:35:36 +0700

    chore: add prettier and format all

--- README.md
+++ README.md
@@ -1,21 +1,25 @@
 # coffee-shop-api
+
 best coffee shop api ever
 
 ## stack
+
 - postgresql
 - express
 - prisma orm
 
 ## todo
+
 1. [x] bash start app script
 2. [x] update schema
-3. [ ] add seed
+3. [x] add seed
 4. [ ] make hello endpoint
-4. [ ] add ant fu lint
+5. [ ] add ant fu lint
 
 ## prerequisites
+
 - docker: [install docker engine on ubuntu](https://docs.docker.com/engine/install/ubuntu/)
 
-
 ## quickstart
+
 just run the `local-start.sh` to run this app in a container, don't forget to `docker compose down` to kill container.
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,20 +1,20 @@
-import { faker } from '@faker-js/faker';
-import { PrismaClient } from '../generated/prisma'
-const prisma = new PrismaClient()
+import { faker } from "@faker-js/faker";
+import { PrismaClient } from "../generated/prisma";
+const prisma = new PrismaClient();
 async function main() {
-    const coffees = Array.from({ length: 10 }).map(() => ({
-        name: faker.commerce.productName() + ' Coffee',
-        description: faker.commerce.productDescription(),
-        price: parseFloat(faker.commerce.price({ min: 2, max: 10 })),
+  const coffees = Array.from({ length: 10 }).map(() => ({
+    name: faker.commerce.productName() + " Coffee",
+    description: faker.commerce.productDescription(),
+    price: parseFloat(faker.commerce.price({ min: 2, max: 10 })),
   }));
-  await prisma.coffee.createMany({data: coffees});
+  await prisma.coffee.createMany({ data: coffees });
 }
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
