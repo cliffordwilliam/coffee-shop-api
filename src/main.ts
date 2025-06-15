77a9d9416fb0f64df3b2bcb98c4f454437ae2d06
Clifford William <ccliffordwilliam@gmail.com>
2025-06-16 01:18:09 +0700

    feat: scaffold prisma client

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
+import { PrismaClient } from "../generated/prisma";
 import express from "express";
+
 const app = express();
 const port = 3000;
+const prisma = new PrismaClient();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/coffees", async (req, res) => {
+  try {
+    const coffees = await prisma.coffee.findMany();
+    res.json(coffees);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Something went wrong.");
+  }
 });
 
 app.listen(port, () => {
