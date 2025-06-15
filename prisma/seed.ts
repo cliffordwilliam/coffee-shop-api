978bb5bfa415e0b2c7953129d10e627fb2958b43
Clifford William <ccliffordwilliam@gmail.com>
2025-06-15 23:50:34 +0700

    feat: add faker seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,13 @@
+import { faker } from '@faker-js/faker';
 import { PrismaClient } from '../generated/prisma'
 const prisma = new PrismaClient()
 async function main() {
-//   const alice = await prisma.coffee
-//   console.log({ alice })
+    const coffees = Array.from({ length: 10 }).map(() => ({
+        name: faker.commerce.productName() + ' Coffee',
+        description: faker.commerce.productDescription(),
+        price: parseFloat(faker.commerce.price({ min: 2, max: 10 })),
+  }));
+  await prisma.coffee.createMany({data: coffees});
 }
 main()
   .then(async () => {
